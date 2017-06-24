df6b3393e21c1ef2fb05a9a404c729acf3bf0a37
Emil Kirilov <emil.kirilovk@gmail.com>
2017-06-24 12:36:21 +0300

    Parallel dfs algorithm.

---
+++ src/Main.java
@@ -0,0 +1,105 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class Main {
+	public static void main(String[] args) throws ParseException {
+		Options options = new Options();
+		options.addOption("q", "quiet", false, "Only execution time is shown.");
+		options.addOption("t", "task", true, "Thread count.");
+		options.addOption("i", true, "Input file.");
+		options.addOption("o", true, "Output file.");
+		options.addOption("n", true, "Matrix size.");
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse(options, args);
+		
+		String inputFile = cmd.getOptionValue("i");
+		String outputFile = cmd.getOptionValue("o");
+		Integer threadCount = Integer.parseInt(cmd.getOptionValue("t"));
+		boolean quietMode = cmd.hasOption("q");
+		String matrixSize = cmd.getOptionValue("n");
+		
+		long start = System.currentTimeMillis();
+//		Scanner s = new Scanner(System.in);
+//		String input = s.nextLine();
+
+		if(inputFile != null){
+			String filename = "/home/emo/Desktop/Work/try.in";
+			BufferedReader br = null;
+			FileReader fr = null;
+			try {
+				String sCurrentLine;
+
+				br = new BufferedReader(new FileReader(filename));
+				int n = Integer.parseInt(br.readLine());
+				boolean[][] n_matrix = new boolean[n][n];
+				HashMap<Integer, Queue<Integer>> matrix = new HashMap<Integer, Queue<Integer>>(n);
+				
+				int row = 0;
+				while ((sCurrentLine = br.readLine()) != null) {
+					
+					String[] connections = sCurrentLine.split("\t"); 							//fix
+					for (int i = 0; i < connections.length; i++) {
+						if(connections[i].equals("1")) {
+							if(!matrix.containsKey(row)) {
+								matrix.put(row, new ConcurrentLinkedQueue<Integer>());
+							}
+							if(row < i) {
+								matrix.get(row).add(i);
+							}
+						}
+					}
+					row++;
+				}
+				
+				System.out.println("Build up time -> " + (System.currentTimeMillis() - start) );
+
+				
+				AtomicInteger firstUntraversed = new  AtomicInteger(0);
+				
+				Worker.setNVertices(n);
+				ExecutorService service = Executors.newFixedThreadPool(threadCount);
+				
+				for (int i = 0; i < threadCount; i++) {
+		            Worker w = new Worker(i, firstUntraversed, matrix);
+		            service.execute(w);
+		        }
+				
+				try {
+					service.awaitTermination( 2000, TimeUnit.NANOSECONDS);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("eeeee");
+			} catch (IOException e) { 
+				e.printStackTrace();
+			} finally {
+				try {
+					if (br != null)
+						br.close();
+					if (fr != null)
+						fr.close();
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+
+			}
+		} else {
+			//Generate file
+		}
+	}
+}
---
+++ src/Worker.java
@@ -0,0 +1,65 @@
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Worker implements Runnable {
+	public static int nVertices;
+	private static Deque<Integer> stack = new ConcurrentLinkedDeque<Integer>();
+	private static Set<Integer> stack_contains = Collections.newSetFromMap(new ConcurrentHashMap<Integer, Boolean>(Worker.nVertices));
+	private static Set<Integer> traversed = Collections.newSetFromMap(new ConcurrentHashMap<Integer, Boolean>(nVertices));
+	private long started;
+	private int id;
+	private static AtomicInteger firstUntraversed;
+	private static HashMap<Integer, Queue<Integer>> matrix;
+
+	
+	public static void setNVertices(int n) {
+		nVertices = n;
+	}
+
+    public Worker(int id) {
+        this.started = System.currentTimeMillis();
+    	this.id = id;
+//    	this.firstUntraversed = firstUntraversed;
+//        this.matrix = matrix;
+    }
+    
+    @Override
+    public void run() {
+    	System.out.println("Thread-" + id + " started.");
+    	
+        while(Worker.traversed.size() < Worker.nVertices) {
+        	Integer current = null;
+        	Integer nextInList = Worker.firstUntraversed.getAndIncrement();
+        	if(nextInList < Worker.nVertices) {
+        		current = nextInList;
+        	} else {
+        		current = Worker.stack.pollLast();
+        	}	
+       		while(current != null) {
+       			if(	Worker.traversed.contains(current) ){
+	           		break;
+	           	} else {
+	           		Integer neighbour = Worker.matrix.get(current).poll();
+	           		if( Worker.matrix.get(current).isEmpty() ) {
+	           			Worker.traversed.add(current);
+	    	           	System.out.println(current);
+	           		}
+	           		if( !stack_contains.contains(current) ){
+	           			Worker.stack.add(current);
+	           		}
+	           		current = neighbour;
+	            }
+        	}
+
+        	
+        }
+        System.out.println("Thread-" + id + " stopped.");
+        System.out.println("Thread-" + id + " execution time was " + (System.currentTimeMillis() - started) + "ms");   
+    }
+}
